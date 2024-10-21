8c02e34376aa85e0404e07bfd7e0e3bb5d5a9033
zhangl <810402961@qq.com>
2024-10-21 23:07:48 +0800

    Init

---
+++ lib/src/listener/connect_listener.dart
@@ -0,0 +1,55 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
+
+/// SDK Connection State Listener
+class OnConnectListener {
+  Function(int? code, String? errorMsg)? onConnectFailed;
+  Function()? onConnectSuccess;
+  Function()? onConnecting;
+  Function()? onKickedOffline;
+  Function()? onUserTokenExpired;
+  Function()? onUserTokenInvalid;
+  Function(String? ip)? onIpNotWhitelist;
+
+  OnConnectListener({
+    this.onConnectFailed,
+    this.onConnectSuccess,
+    this.onConnecting,
+    this.onKickedOffline,
+    this.onUserTokenExpired,
+    this.onUserTokenInvalid,
+    this.onIpNotWhitelist
+  });
+
+  /// SDK failed to connect to the server
+  void connectFailed(int? code, String? errorMsg) {
+    onConnectFailed?.call(code, errorMsg);
+  }
+
+  /// SDK successfully connected to the server
+  void connectSuccess() {
+    onConnectSuccess?.call();
+  }
+
+  /// SDK is currently connecting to the server
+  void connecting() {
+    onConnecting?.call();
+  }
+
+  /// The account has been logged in from another location, and the current device has been kicked offline
+  void kickedOffline() {
+    onKickedOffline?.call();
+  }
+
+  /// Login credentials have expired and require reauthentication
+  void userTokenExpired() {
+    onUserTokenExpired?.call();
+  }
+
+  void userTokenInvalid() {
+    onUserTokenInvalid?.call();
+  }
+
+  void OnIpNotWhitelist(String? s){
+    onIpNotWhitelist?.call(s);
+  }
+}
